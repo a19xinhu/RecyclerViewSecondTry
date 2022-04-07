72f42f40139ab46faed22e0ba3692d4cdcc73aee
a19xinhu <63045875+a19xinhu@users.noreply.github.com>
2022-04-07 09:46:52 +0200

    add sample data

--- app/src/main/java/com/example/recyclerviewsecondtry/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewsecondtry/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.recyclerviewsecondtry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> mountains = Arrays.asList("Kinnekulle","Billingen","K2","Kebenikaise","Hannerberg","Hunneberg");
+
+        recyclerView = findViewById(R.id.recycler_view);
+
     }
 }
